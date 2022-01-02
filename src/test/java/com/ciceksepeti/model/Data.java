150097f2fc4efd5961bf3254b5ac6cd4acc48cad
canoklay <fatihcan.oklay@vigo.ist>
2022-01-02 22:25:54 +0300

    base page

--- src/test/java/com/ciceksepeti/model/Data.java
+++ src/test/java/com/ciceksepeti/model/Data.java
@@ -1,6 +1,5 @@
 package com.ciceksepeti.model;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Data {
--- src/test/java/com/ciceksepeti/test/IntegrationTest.java
+++ src/test/java/com/ciceksepeti/test/IntegrationTest.java
@@ -1,9 +1,9 @@
 package com.ciceksepeti.test;
 
-import com.ciceksepeti.service.Service;
+import com.ciceksepeti.base.BasePage;
 import org.testng.annotations.Test;
 
-public class IntegrationTest extends Service {
+public class IntegrationTest extends BasePage {
 
     @Test(testName = "Installment değerine göre dönen sonuçların kontrol edilmesi")
     public void installmentTest() {
