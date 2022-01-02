47f953463fe39126b63b51afc8aa7297ae70f16e
canoklay <fatihcan.oklay@vigo.ist>
2022-01-02 19:30:07 +0300

    begin first case

--- src/test/java/com/ciceksepeti/model/Response.java
+++ src/test/java/com/ciceksepeti/model/Response.java
@@ -1,4 +1,27 @@
 package com.ciceksepeti.model;
 
+import java.util.List;
+
 public class Response {
+
+    private List<String> products;
+    private String productGroupId;
+    private String installmentText;
+    private boolean installment;
+
+    public List<String> getProducts() {
+        return products;
+    }
+
+    public String getProductGroupId() {
+        return productGroupId;
+    }
+
+    public String getInstallmentText() {
+        return installmentText;
+    }
+
+    public boolean getInstallment() {
+        return installment;
+    }
 }
